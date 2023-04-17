50158c24d66785e973e4d79dd35ef02b8d45670c
Rodrigo <brokenb1312@hotmail.com>
2023-04-17 18:06:04 -0300

    adding a limit of 5 emails per day and a 2 minutes limit between them

--- client/src/screens/LoginHelp.tsx
+++ client/src/screens/LoginHelp.tsx
@@ -1,6 +1,7 @@
 import { useState, useRef } from 'react';
 
 import { motion } from 'framer-motion';
+import Countdown from 'react-countdown';
 import { useNavigate } from "react-router-dom";
 import { useForm, FieldValues } from 'react-hook-form';
 
@@ -10,9 +11,8 @@ import forgotPassword from '../assets/forgot-password.svg';
 import { toastAlert } from '../components/Alert/Alert';
 import api from '../services/api';
 
-type Props = {}
-
-export default function LoginHelp({}: Props) {
+export default function LoginHelp() {
+  const [ timer, setTimer ] = useState('');
   const [ loader, setLoader ] = useState(false);
   const [ wasChanged, setWasChanged ] = useState(false);
   const [ triggerCode, setTriggerCode ] = useState<boolean | string>(false);
@@ -24,7 +24,6 @@ export default function LoginHelp({}: Props) {
 
   const getEmail = async (data: FieldValues) => {
     setLoader(true);
-    // reset({email: ''});
   
     try {
       const findUser = await api.post('https://noap-typescript-api.vercel.app/find-user', {
@@ -44,8 +43,9 @@ export default function LoginHelp({}: Props) {
       toastAlert({icon: 'success', title: `${findUser.data.message}`, timer: 2000});
     } catch (err: any) {
       setLoader(false);
-      console.log(err);
-      toastAlert({icon: 'error', title: `${err.response.data.message}`, timer: 2000});
+      
+      setTimer(err.response.data.spam ? err.response.data.spam : 'maximum');
+      toastAlert({icon: 'error', title: `${err.response.data.message}`, timer: 2500});
     }
   };
 
@@ -111,6 +111,44 @@ export default function LoginHelp({}: Props) {
     }
   }
 
+  const renderer = ({ minutes, seconds, completed }: any) => {
+    if (!completed) {
+      return (
+        <button 
+          className={`bg-stone-700 hover:bg-stone-800 rounded-full !mt-5 py-2 text-sm uppercase tracking-widest transition-all duration-500 ease-in-out`}
+        >
+          <div className={`flex flex-row justify-center py-[4px]`}>
+            <span className='pt-0 xxs:pt-[2.4px]'>
+              <p className='!text-xs'>
+                  you can send another email in {minutes} minute(s) : {seconds} second(s)
+              </p>  
+            </span>
+            <svg aria-hidden="true" role="status" className="inline w-4 h-4 ml-3 text-white animate-spin xxs:my-1 my-[1.5px]" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
+            </svg>
+          </div>
+        </button>
+      )
+    }
+    else return (
+      <button 
+        className={`bg-red-700 hover:bg-red-800 rounded-full !mt-5 py-2 text-sm uppercase tracking-widest transition-all duration-500 ease-in-out`}
+      >
+        <div className={`${!loader && 'hidden'} flex flex-row justify-center py-[1px]`}>
+          <svg aria-hidden="true" role="status" className="inline w-4 h-4 mr-3 text-white animate-spin xxs:my-1 my-[1.5px]" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
+          </svg>
+          <span className='pt-0 xxs:pt-[2.4px]'>
+            Loading...  
+          </span>
+        </div>
+        <p className={`${loader && 'hidden'}`}>Send code</p>
+      </button>
+    )
+  };
+
   return (
     <div className="!bg-gray-800 h-screen overflow-scroll">
       <div className="flex flex-row justify-between w-screen bg-stone-900 pb-3 pt-1 fixed z-10">
@@ -149,25 +187,52 @@ export default function LoginHelp({}: Props) {
                         <input 
                           type="email"
                           required 
-                          className='sign-text-inputs bg-stone-900 text-gray-300 border-transparent active:border focus:border-gray-400'
+                          className={`sign-text-inputs bg-stone-900 text-gray-300 border-transparent active:border focus:border-gray-400 ${timer === "maximum" && "border !border-red-700 hover:!border-red-800"}`}
                           placeholder='Email address'
                           autoComplete='off'
                           {...register('email')}
                         />
-                        <button 
-                          className={`bg-red-700 hover:bg-red-800 rounded-full !mt-5 py-2 text-sm uppercase tracking-widest transition-all duration-500 ease-in-out`}
-                        >
-                          <div className={`${!loader && 'hidden'} flex flex-row justify-center py-[1px]`}>
-                            <svg aria-hidden="true" role="status" className="inline w-4 h-4 mr-3 text-white animate-spin xxs:my-1 my-[1.5px]" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
-                              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
-                            </svg>
-                            <span className='pt-0 xxs:pt-[2.4px]'>
-                              Loading...  
-                            </span>
-                          </div>
-                          <p className={`${loader && 'hidden'}`}>Send code</p>
-                        </button>
+                        {timer !== 'maximum' && timer !== '' ? (
+                            <Countdown 
+                              date={timer}
+                              renderer={renderer}
+                            />
+                        ) : timer === 'maximum' ? (
+                          <>
+                            <p className='!mt-5 !mb-2 text-xs uppercase tracking-widest text-red-600 px-2'>Maximum number of emails exceeded and you will not be able to send more emails. Please wait 24 hours to send new emails!</p>
+                            <button 
+                              className='rounded-full py-2 text-sm uppercase tracking-widest transition-all duration-500 ease-in-out disabled:bg-gray-600 hover:cursor-not-allowed opacity-60'
+                              disabled={true}
+                              type='button'
+                            >
+                              <div className={`${!loader && 'hidden'} flex flex-row justify-center py-[1px]`}>
+                                <svg aria-hidden="true" role="status" className="inline w-4 h-4 mr-3 text-white animate-spin xxs:my-1 my-[1.5px]" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
+                                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
+                                </svg>
+                                <span className='pt-0 xxs:pt-[2.4px]'>
+                                  Loading...  
+                                </span>
+                              </div>
+                              <p className={`${loader && 'hidden'}`}>Send code</p>
+                            </button>
+                          </>
+                        ) : (
+                          <button 
+                            className={`bg-red-700 hover:bg-red-800 rounded-full !mt-5 py-2 text-sm uppercase tracking-widest transition-all duration-500 ease-in-out`}
+                          >
+                            <div className={`${!loader && 'hidden'} flex flex-row justify-center py-[1px]`}>
+                              <svg aria-hidden="true" role="status" className="inline w-4 h-4 mr-3 text-white animate-spin xxs:my-1 my-[1.5px]" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
+                                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
+                              </svg>
+                              <span className='pt-0 xxs:pt-[2.4px]'>
+                                Loading...  
+                              </span>
+                            </div>
+                            <p className={`${loader && 'hidden'}`}>Send code</p>
+                          </button>
+                        )}                        
                       </>
                     ) : (
                       <>
@@ -192,6 +257,13 @@ export default function LoginHelp({}: Props) {
                             </span>
                           </div>
                           <p className={`${loader && 'hidden'}`}>Verify code</p>
+                        </button>
+                        <button 
+                          type='button'
+                          className='border border-gray-600 hover:bg-stone-800 rounded-full !mt-2 py-2 text-sm uppercase tracking-widest hover:!text-[13px] transition-all duration-500 ease-in-out'
+                          onClick={() => setTriggerCode(false)}
+                        >
+                          Didn't recive the code ?
                         </button>
                       </>
                     )}
