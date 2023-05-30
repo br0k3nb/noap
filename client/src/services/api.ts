c0fe3d9098b01c6768757aa11814a50f4a8fcf88
Rodrigo <brokenb1312@hotmail.com>
2023-05-30 02:39:35 -0300

    adding vercel endpoints

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const config =  { host: 'localhost', port: 3001 };
-const { host, port } = config;
+// const config =  { host: 'localhost', port: 3001 };
+// const { host, port } = config;
 
-const api = axios.create({baseURL: ` http://${host}:${port}/`});
+// const api = axios.create({baseURL: ` http://${host}:${port}/`});
 
-// const api = axios.create({baseURL: `https://noap-typescript-api.vercel.app/`});
+const api = axios.create({baseURL: `https://noap-typescript-api.vercel.app/`});
 
 export default api;
