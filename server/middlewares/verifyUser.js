f0e8f6597cf0692462f476d13ebc712ed0154cbe
Rodrigo <brokenb1312@hotmail.com>
2023-05-31 13:07:34 -0300

    fixing token middleware

--- server/middlewares/verifyUser.js
+++ server/middlewares/verifyUser.js
@@ -2,13 +2,23 @@ import jwt from "jsonwebtoken";
 import 'dotenv/config';
 
 export default (req, res, next) => {
-  
-  const authToken = req.params.token;
+  const authorization = req.headers.authorization;
 
-  // if(!authToken) return res.status(400).json({message: "Access denied, sign in again!"});
+  if (!authorization) return res.status(401).json({ message: "Authentication token wasn't found" });
 
-  jwt.verify(authToken, `${process.env.SECRET}`, err => {
-    // if (err) return res.status(400).json({ message: "Access denied, sign in again!" });
-     return next();
+  const parts = authorization.split(' ');
+
+  if (!parts.length === 2) return res.status(401).json({ message: "Authentication error" });
+
+  const [scheme, data] = parts;
+
+  if (!/^Bearer$/i.test(scheme)) return res.status(401).json({ message: "Invalid token" });
+
+  const { token } = JSON.parse(data);
+
+  jwt.verify(token, `${process.env.SECRET}`, (err, decoded) => {
+    if (err) return res.status(401).send({ message: "Access denied, sign in again" });
+    
+    return next();
   });
 };
