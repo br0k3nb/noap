599beab498c94441e87a7e9eba3139aa41504683
Rodrigo Oliveira <brokenb1312@hotmail.com>
2023-06-19 02:13:41 -0300

    creating a settings modal and adding a option to show pinned notes in a folder like structure

--- client/src/screens/Notes/CardNotes.tsx
+++ client/src/screens/Notes/CardNotes.tsx
@@ -1,4 +1,4 @@
-import { useContext, SetStateAction, Dispatch } from "react";
+import { useState, useContext, SetStateAction, Dispatch } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { FieldArrayWithId } from "react-hook-form";
 
@@ -10,6 +10,7 @@ import ghost from '../../assets/ghost.png';
 import Loader from "../../components/Loader";
 
 import { BsFillPinAngleFill } from "react-icons/bs";
+import { MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowUp } from 'react-icons/md';
 
 type Props = {
     page: number;
@@ -24,6 +25,8 @@ export default function CardNotes({ notes, addNewNote, isFetching, setExpanded, 
     const noteContext = useContext(NoteCtx);
     const location = useLocation();
 
+    const [pinWasClicked, setPinWasClicked] = useState(false);
+
     const baseURL = location.pathname.slice(0, (location.pathname.length - String(page).length));
 
     const hours = (date: string) => moment(date).format("LT");
@@ -33,6 +36,10 @@ export default function CardNotes({ notes, addNewNote, isFetching, setExpanded, 
         noteContext?.setSelectedNote(_id);
         setExpanded(window.outerWidth <= 1030 ? true : false);
     };
+
+    // const handlePinnedNotesClick = () => {
+    //     setPinWasClicked(true);
+    // }
 
     const unpinnedNotes = notes.filter(note => !note?.settings?.pinned);
     const pinnedNotes = notes.filter(note => note?.settings && note?.settings.pinned);
@@ -48,31 +55,62 @@ export default function CardNotes({ notes, addNewNote, isFetching, setExpanded, 
                     <>
                         {notes.length > 0 ? (
                             <div className="flex flex-row flex-wrap px-2 my-5 gap-y-6 gap-x-3 mb-48">  
-                                {/* <div className="w-full"> */}
-                                    {pinnedNotes.length > 0 && (
-                                        <div className="mb-10">
-                                            <div className="mt-2 mb-8 flex flex-row space-x-2 justify-center items-center text-gray-300">
-                                                <p className="uppercase text-xs tracking-widest">Pinned notes</p>
-                                                <BsFillPinAngleFill />
-                                            </div>
-                                            <div className="flex flex-row flex-wrap px-2 my-5 gap-y-6 gap-x-3">
-                                                {pinnedNotes.map((pinnedNotes, idx) => {
-                                                    return (
-                                                        <Cards  
-                                                            key={pinnedNotes._id}
-                                                            notes={pinnedNotes}
-                                                            idx={idx}
-                                                            noteContext={noteContext}
-                                                            handleNoteClick={handleNoteClick}
-                                                            days={days}
-                                                            hours={hours}
-                                                        />
-                                                    )
-                                                })}
+                                {pinnedNotes.length > 0 && (
+                                    <>
+                                        <div className="mb-3 mt-2">
+                                            <div className="collapse border border-base-300 bg-gray-900 rounded-lg !w-[360px]">
+                                                <input 
+                                                    readOnly
+                                                    type="checkbox"
+                                                    onClick={() => setPinWasClicked(!pinWasClicked)}
+                                                />
+                                                <div className="collapse-title">
+                                                    <div className="mt-2 mb-2 flex flex-row space-x-2 justify-center items-center text-gray-300">
+                                                        <p className="uppercase text-xs tracking-widest">Pinned notes</p>
+                                                        <BsFillPinAngleFill />
+                                                        <div className="absolute right-2">
+                                                            {pinWasClicked ? (
+                                                                <MdOutlineKeyboardArrowDown 
+                                                                    size={26} 
+                                                                    className="my-auto"
+                                                                />
+                                                            ) : (
+                                                                <MdOutlineKeyboardArrowUp 
+                                                                    size={26} 
+                                                                    className="my-auto"
+                                                                />
+                                                            )}
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="collapse-content">
+                                                    <div className="flex flex-row flex-wrap px-2 my-5 gap-y-6 gap-x-3">
+                                                        {pinnedNotes.map((pinnedNotes, idx) => {
+                                                            return (
+                                                                <Cards  
+                                                                    key={pinnedNotes._id}
+                                                                    idx={idx}
+                                                                    notes={pinnedNotes}
+                                                                    noteContext={noteContext}
+                                                                    customWidth={"!w-[150.5px]"}
+                                                                    handleNoteClick={handleNoteClick}
+                                                                    days={days}
+                                                                    hours={hours}
+                                                                />
+                                                            )
+                                                        })} 
+                                                        <div className="flex flex-row items-center justify-center w-full mt-5">
+                                                            <p className="text-gray-300 uppercase text-xs tracking-wide cursor-pointer hover:tracking-widest duration-300 border border-gray-600 py-2 px-3 rounded-full hover:bg-gray-800">
+                                                                Show more items
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
-                                    )}
-                                {/* </div> */}
+                                        <div className="w-[330px] border border-transparent border-t-gray-600 mb-3 mx-auto"/>
+                                    </>
+                                )}
                                 {unpinnedNotes.map((unpinnedNotes, idx) => {
                                     return (
                                         <Cards  
@@ -123,13 +161,14 @@ type CardsProps = {
     notes: any;
     idx: number;
     noteContext: any;
+    customWidth?: number | string;
     days: (date: string) => string;
     hours: (date: string) => string;
     handleNoteClick:  (_id: string) => void;
 
 };
 
-export function Cards ({ notes, idx, noteContext, handleNoteClick, days, hours }: CardsProps) {
+export function Cards ({ notes, idx, noteContext, handleNoteClick, days, hours, customWidth}: CardsProps) {
     const { image, labels, _id, body, createdAt, updatedAt, title } = notes;
     const { color, type, fontColor, name } = labels[0] || {};
 
@@ -143,11 +182,17 @@ export function Cards ({ notes, idx, noteContext, handleNoteClick, days, hours }
                 className={`
                     rounded-lg h-[18.4rem] w-[165px] xxs:w-[161px] border border-stone-900 bg-gray-700 pt-3 shadow-lg shadow-gray-900 hover:border transition duration-300 hover:border-gray-500
                     ${noteContext?.selectedNote === _id && "!border-gray-300"}
+                    ${customWidth && customWidth}
                 `}
             >
                 <p className="text-lg px-4 mb-3 truncate">{title}</p>
                 <div className={`h-[196px] text-gray-300 flex flex-col px-4 ${image !== '' && "!h-[148px]"}`}>
-                    <div className="!w-[135px] overflow-ellipsis overflow-hidden !mb-1">
+                    <div 
+                        className={`
+                            !w-[135px] overflow-ellipsis overflow-hidden !mb-1
+                            ${customWidth && "!w-[119.5px]"}
+                        `}
+                    >
                         {/* using this conditions because for some reason the string truncation isn't working properly */}
                         {
                             labels.length && image === '' ? (body.length >= 135 ? body.slice(0,114).concat('...') : body) :
@@ -191,11 +236,21 @@ export function Cards ({ notes, idx, noteContext, handleNoteClick, days, hours }
                         </div>
                     )}
                 </div>
-                <p className="text-xs tracking-tighter mt-2 px-4 pb-[0.04rem]">
+                <p 
+                    className={`
+                        text-xs tracking-tighter mt-2 px-4 pb-[0.04rem]
+                        ${customWidth && "!text-[11px]"}
+                    `}
+                >
                     {!updatedAt ? days(createdAt) + " at " + hours(createdAt) : days(updatedAt) + " at " + hours(updatedAt)}
                 </p>
                 {image !== '' && (
-                    <div className="h-[56px] mt-3 w-[165px] xxs:w-[161px] rounded-b-lg">
+                    <div 
+                        className={`
+                            h-[56px] mt-3 w-[165px] xxs:w-[161px] rounded-b-lg
+                            ${customWidth && customWidth}
+                        `}
+                    >
                         <img 
                             src={image}
                             className="rounded-b-[6.5px] object-cover !h-[3.50rem] min-w-[98.9%]"
